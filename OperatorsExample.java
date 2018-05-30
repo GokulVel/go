e15f3f59d58c744599c1e9fda8011917cf88e316
GokulVel <gokulsmart449@gmail.com>
2018-05-30 02:47:43 -0700

    h

---
+++ OperatorsExample.java
@@ -0,0 +1,11 @@
+package com.java.operators;
+public class OperatorsExample {
+public static void main(String[] args) {
+int count = 10;
+count++;
+count--;
+int negCount = -20;
+boolean isTrue = false;
+System.out.println(!isTrue);
+}
+}

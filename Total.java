fa73930a50fca325df47c8961a66ccb3db6b9167
GokulVel <gokulsmart449@gmail.com>
2018-05-30 03:52:17 -0700

    m

---
+++ Total.java
@@ -0,0 +1,16 @@
+public class Total 
+{
+public static void main (String[] args) 
+{
+        int x = 1,
+        Total = 5;
+
+	while ( x <= 10 )
+	{
+		Total += x;
+		++x;
+		System.out.println(Total);
+		
+	}
+}
+}

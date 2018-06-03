681396460d05c11e862ae442b0da5b94c1dcd748
GokulVel <gokulsmart449@gmail.com>
2018-06-03 04:51:52 -0700

    unary

---
+++ Run.java
@@ -0,0 +1,16 @@
+public class Run
+{
+public static void main (String[] args) 
+{
+        int x = 5;
+        
+
+	while ( x <= 100 )
+	{
+		Total += x;
+		++x;
+		System.out.println(Total);
+		
+	}
+}
+}
